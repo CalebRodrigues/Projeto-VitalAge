3fbe8185ef1488c22f8f30e0731f06a9872c1701
Caleb Rodrigues <c4l3b23@gmail.com>
2023-10-26 16:14:09 -0300

    update

--- site/js/script-registro.js
+++ site/js/script-registro.js
@@ -21,87 +21,106 @@ if (user) {
     var batimentoData = snapshot.val();
     var batimento = batimentoData.bpm;
     var timestamp = batimentoData.timestamp;
-  
+
     var listItem = document.createElement("li");
-  
+
+    // Div para envolver o ícone de coração e o BPM
+    var heartBpmDiv = document.createElement("div");
+    heartBpmDiv.className = "heart-bpm-container animate__animated animate__fadeInUp animate__delay-0.1s";
+
     // Criar a imagem de coração
     var heartIcon = document.createElement("div");
-    heartIcon.className = "heart-icon";
+    heartIcon.className = "heart-icon animate__animated animate__fadeInUp animate__delay-0.1s";
     var img = document.createElement("img");
     img.src = "./img/coracao-registros.png";
     img.alt = "Coração";
     heartIcon.appendChild(img);
-  
+
     // Criar elemento para exibir o BPM
     var bpmElement = document.createElement("span");
-    bpmElement.className = "bpm";
+    bpmElement.className = "bpm animate__animated animate__fadeInUp animate__delay-0.1s";
     bpmElement.textContent = batimento;
-  
-    // Criar ícone de relógio
-    var clockIcon = document.createElement("div");
-    clockIcon.className = "clock-icon";
-    var clockImg = document.createElement("img");
-    clockImg.src = "./img/relogio.png"; // Substitua pelo caminho do ícone de relógio
-    clockImg.alt = "Relógio";
-    clockIcon.appendChild(clockImg);
-  
-    // Exibir o horário do timestamp
-    var timeElement = document.createElement("span");
-    timeElement.className = "time";
-    timeElement.textContent = formatDate(timestamp).split(" - ")[1]; // Para exibir apenas o horário
-  
+
+    heartBpmDiv.appendChild(heartIcon);
+    heartBpmDiv.appendChild(bpmElement);
+
+    // Div para envolver o ícone de calendário e a data
+    var calendarDateDiv = document.createElement("div");
+    calendarDateDiv.className = "calendar-date-container animate__animated animate__fadeInUp animate__delay-0.1s";
+
     // Criar ícone de calendário
     var calendarIcon = document.createElement("div");
-    calendarIcon.className = "calendar-icon";
+    calendarIcon.className = "calendar-icon animate__animated animate__fadeInUp animate__delay-0.1s";
     var calendarImg = document.createElement("img");
     calendarImg.src = "./img/calendario.png"; // Substitua pelo caminho do ícone de calendário
     calendarImg.alt = "Calendário";
     calendarIcon.appendChild(calendarImg);
-  
+
     // Exibir a data do timestamp
     var dateElement = document.createElement("span");
-    dateElement.className = "date";
+    dateElement.className = "date animate__animated animate__fadeInUp animate__delay-0.1s";
     dateElement.textContent = formatDate(timestamp).split(" - ")[0]; // Para exibir apenas a data
-  
+
+    calendarDateDiv.appendChild(calendarIcon);
+    calendarDateDiv.appendChild(dateElement);
+
+    // Div para envolver o ícone de relógio e a hora
+    var clockTimeDiv = document.createElement("div");
+    clockTimeDiv.className = "clock-time-container animate__animated animate__fadeInUp animate__delay-0.1s";
+
+    // Criar ícone de relógio
+    var clockIcon = document.createElement("div");
+    clockIcon.className = "clock-icon animate__animated animate__fadeInUp animate__delay-0.1s";
+    var clockImg = document.createElement("img");
+    clockImg.src = "./img/relogio.png"; // Substitua pelo caminho do ícone de relógio
+    clockImg.alt = "Relógio";
+    clockIcon.appendChild(clockImg);
+
+    // Exibir o horário do timestamp
+    var timeElement = document.createElement("span");
+    timeElement.className = "time animate__animated animate__fadeInUp animate__delay-0.1s";
+    timeElement.textContent = formatDate(timestamp).split(" - ")[1]; // Para exibir apenas o horário
+
+    clockTimeDiv.appendChild(clockIcon);
+    clockTimeDiv.appendChild(timeElement);
+
     // Criar ícone de lixeira
-  var deleteIcon = document.createElement("div");
-  deleteIcon.className = "delete-icon";
-  var trashImg = document.createElement("img");
-  trashImg.src = "./img/lixo.png";
-  trashImg.alt = "Apagar";
-  deleteIcon.appendChild(trashImg);
+    var deleteIcon = document.createElement("div");
+    deleteIcon.className = "delete-icon animate__animated animate__fadeInUp animate__delay-0.1s";
+    var trashImg = document.createElement("img");
+    trashImg.src = "./img/lixo.png";
+    trashImg.alt = "Apagar";
+    deleteIcon.appendChild(trashImg);
 
-  // Adicionar um evento de clique ao ícone de lixeira
-  deleteIcon.addEventListener("click", function() {
-    // Obtém a chave do registro a ser excluído
-    var recordKey = snapshot.key;
+    // Adicionar um evento de clique ao ícone de lixeira
+    deleteIcon.addEventListener("click", function() {
+        // Obtém a chave do registro a ser excluído
+        var recordKey = snapshot.key;
 
-    // Referência ao registro no banco de dados
-    var recordRef = batimentosRef.child(recordKey);
+        // Referência ao registro no banco de dados
+        var recordRef = batimentosRef.child(recordKey);
 
-    // Remove o registro do banco de dados
-    recordRef.remove()
-      .then(function() {
-        // Remove o item da lista na interface do usuário
-        listItem.remove();
-      })
-      .catch(function(error) {
-        console.error("Erro ao excluir o registro:", error);
-      });
-  });
-  
+        // Remove o registro do banco de dados
+        recordRef.remove()
+            .then(function() {
+                // Remove o item da lista na interface do usuário
+                listItem.remove();
+            })
+            .catch(function(error) {
+                console.error("Erro ao excluir o registro:", error);
+            });
+    });
+
     // Adicionar os elementos à lista
-    listItem.appendChild(heartIcon);
-    listItem.appendChild(bpmElement);
-    listItem.appendChild(calendarIcon);
-    listItem.appendChild(dateElement);
-    listItem.appendChild(clockIcon);
-    listItem.appendChild(timeElement);
+    listItem.appendChild(heartBpmDiv);
+    listItem.appendChild(calendarDateDiv);
+    listItem.appendChild(clockTimeDiv);
     listItem.appendChild(deleteIcon);
-  
+
     var heartRateList = document.getElementById("heart-rate-list");
     heartRateList.appendChild(listItem);
-  });
+});
+
   
 
   var firestore = firebase.firestore();
